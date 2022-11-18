2bdad3a133ed09e69481385ccfd685a1e757c00b
Victor Duran <victor.duran.em@gmail.com>
2022-11-18 13:56:08 -0500

    Add dimensions to profile picture

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -7,9 +7,9 @@ type ProfilePictureProps = {
 export const ProfilePicture: Component<ProfilePictureProps> = (props) => {
   return (
     <picture class={`${props.className}`}>
-      <source srcset="/image-profile-desktop.webp" media="(min-width: 1280px)" />
-      <source srcset="/image-profile-tablet.webp" media="(min-width: 768px)" />
-      <img src="/image-profile-mobile.webp" alt="profile picture" class={`${props.className}`}  />
+      <source srcset="/image-profile-desktop.webp" media="(min-width: 1280px)" width="890" height="1440" />
+      <source srcset="/image-profile-tablet.webp" media="(min-width: 768px)" width="646" height="1200" />
+      <img src="/image-profile-mobile.webp" alt="profile picture" class={`${props.className}`} width="348" height="766"  />
     </picture>
   );
 };
